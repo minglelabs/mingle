adb0ca8330204e9c124f9094a5c33e97877f6b6d
NamHyeongKeol <grey_pastor@naver.com>
2026-04-02 18:13:12 +0900

    tune: shorten mic error retry delay

--- mingle-app/src/components/LivePhoneDemo/use-realtime-stt.ts
+++ mingle-app/src/components/LivePhoneDemo/use-realtime-stt.ts
@@ -19,7 +19,7 @@ export const getWsUrl = (): string => {
   return `${protocol}://${host}:${WS_PORT}`
 }
 const DEFAULT_USAGE_LIMIT_SEC = 60
-const CONNECTION_ERROR_RESET_DELAY_MS = 2_000
+const CONNECTION_ERROR_RESET_DELAY_MS = 1_000
 
 const LS_KEY_UTTERANCES = 'mingle_demo_utterances'
 const LS_KEY_USAGE = 'mingle_demo_usage_sec'
