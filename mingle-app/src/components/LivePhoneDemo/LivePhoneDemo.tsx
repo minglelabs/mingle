1ab5b82a370d7552cad2afdb39926656d9e39d1b
NamHyeongKeol <grey_pastor@naver.com>
2026-04-08 02:07:45 +0900

    Increase composer send icon size

--- mingle-app/src/components/LivePhoneDemo/LivePhoneDemo.tsx
+++ mingle-app/src/components/LivePhoneDemo/LivePhoneDemo.tsx
@@ -4091,7 +4091,7 @@ const LivePhoneDemo = forwardRef<LivePhoneDemoRef, LivePhoneDemoProps>(function 
                       <svg
                         aria-hidden
                         viewBox="0 0 24 24"
-                        className="h-[1.05rem] w-[1.05rem]"
+                        className="h-[1.55rem] w-[1.55rem]"
                         fill="none"
                         stroke="currentColor"
                         strokeWidth="2.4"
