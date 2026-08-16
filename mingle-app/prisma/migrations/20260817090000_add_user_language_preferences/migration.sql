2891c2610ce74508589f4b214ef87b987d5953ad
NamHyeongKeol <grey_pastor@naver.com>
2026-08-17 00:54:14 +0900

    Qualify user language migration schema

--- mingle-app/prisma/migrations/20260817090000_add_user_language_preferences/migration.sql
+++ mingle-app/prisma/migrations/20260817090000_add_user_language_preferences/migration.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "app_users"
+ALTER TABLE "app"."app_users"
 ADD COLUMN "primary_languages" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
 ADD COLUMN "default_conversation_languages" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[];
